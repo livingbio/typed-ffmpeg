f5ead9a92fa6c72718aa26ecbf13616ed62b8e9b
David Chen <lucemia@gmail.com>
2023-12-08 16:29:51 +0800

    add split_documents command (#9)

--- scripts/main.py
+++ scripts/main.py
@@ -1,17 +1,45 @@
 # https://ffmpeg.org/ffmpeg-filters.html
+
+import pathlib
+import re
 import urllib.request
 
 import typer
 
+app = typer.Typer()
 
+DOCUMENT_PATH = pathlib.Path("./source")
+
+
+@app.command()
 def download_ffmpeg_filter_documents() -> None:
+    # download ffmpeg filter documents
     url = "https://ffmpeg.org/ffmpeg-filters.html"
     response = urllib.request.urlopen(url)
     data = response.read()
     text = data.decode("utf-8")
-    with open("ffmpeg-filters.html", "w") as f:
-        f.write(text)
+
+    with (DOCUMENT_PATH / "ffmpeg-filters.html").open("w") as ofile:
+        ofile.write(text)
+
+
+@app.command()
+def split_documents() -> None:
+    # split documents into individual files for easier processing
+
+    pattern = re.compile(r'(?P<body><h3 class="section"><a href="(.*?)">(?P<name>.*?)</a></h3>(.*?))<span', re.MULTILINE | re.DOTALL)
+
+    def extract_filter(html: str) -> list[tuple[str, str]]:
+        return [(m.group("name"), m.group("body")) for m in pattern.finditer(html)]
+
+    with (DOCUMENT_PATH / "ffmpeg-filters.html").open() as ifile:
+        for name, body in extract_filter(ifile.read()):
+            with open(f"source/{name}.html", "w") as ofile:
+                ofile.write(body)
+
+    return None
 
 
 if __name__ == "__main__":
-    typer.run(download_ffmpeg_filter_documents)
+    DOCUMENT_PATH.mkdir(exist_ok=True)
+    app()
