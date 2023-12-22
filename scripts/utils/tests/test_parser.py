ab8c2cb9e6e2722e00b9029506a03f17405158ca
lucemia <lucemia@gmail.com>
2023-12-22 10:06:54 +0800

    fix snapshot

--- scripts/utils/tests/test_parser.py
+++ scripts/utils/tests/test_parser.py
@@ -2,15 +2,18 @@ import pathlib
 
 import pytest
 from syrupy.assertion import SnapshotAssertion
+from syrupy.extensions.json import JSONSnapshotExtension
 from syrupy.filters import props
 
-from ..parser import parse_filter_document
+from ..parser import FilterDocument
 
 test_data = pathlib.Path(__file__).parent / "data"
 
 
 @pytest.mark.parametrize("filepath", test_data.glob("*.html"), ids=lambda x: x.name)
 def test_parse_filter_document(snapshot: SnapshotAssertion, filepath: pathlib.Path) -> None:
-    with filepath.open() as ifile:
-        body = ifile.read()
-    assert snapshot(exclude=props("path", "refs")) == parse_filter_document(body).model_dump(mode="json")
+    filter_document = FilterDocument.load(filepath)
+    assert snapshot(exclude=props("path", "refs"), extension_class=JSONSnapshotExtension) == filter_document.model_dump(
+        mode="json"
+    )
+    assert snapshot(extension_class=JSONSnapshotExtension) == filter_document.parameters
