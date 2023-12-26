e6454350c1ec0a5826cb6228d9c3eb8a192c29bc
lucemia <lucemia@gmail.com>
2023-12-26 13:16:01 +0800

    init code gen

---
+++ scripts/code_gen/gen.py
@@ -0,0 +1,28 @@
+import pathlib
+from pathlib import Path
+
+import jinja2
+
+from .schema import FFmpegFilter
+
+template_path = Path(__file__).parent / "templates"
+
+loader = jinja2.FileSystemLoader(template_path)
+env = jinja2.Environment(
+    loader=loader,
+)
+
+
+def render(filters: list[FFmpegFilter], outpath: pathlib.Path) -> list[pathlib.Path]:
+    output = []
+    for template_file in template_path.glob("*.py.jinja"):
+        template = env.get_template(template_file.name)
+        code = template.render(filters=filters)
+
+        opath = outpath / template_file.name.replace(".jinja", "")
+        with opath.open("w") as ofile:
+            ofile.write(code)
+
+        output.append(opath)
+
+    return output
