04578dfad2440e3dd6dd4b59b6db76dbc117ab0a
lucemia <lucemia@gmail.com>
2023-12-28 22:01:47 +0800

    fix

--- src/ffmpeg/base.py
+++ src/ffmpeg/base.py
@@ -7,16 +7,17 @@ if TYPE_CHECKING:
 
 
 class Stream(BaseModel):
-    node: "FilterNode" | None = None
+    node: "Node" | None = None
     label: str | None = None
     selector: str | None = None
 
 
-class FilterNode(BaseModel):
-    streams: list[Stream]
+class Node(BaseModel):
     name: str
     kwargs: dict[str, str | int | float | bool | None] = {}
 
+
+class InputNode(Node):
     def _vs(self, label: str = None, selector: str = None) -> "VideoStream":
         from .stream import VideoStream
 
@@ -26,3 +27,23 @@ class FilterNode(BaseModel):
         from .stream import AudioStream
 
         return AudioStream(node=self, label=label, selector=selector)
+
+
+class FilterNode(InputNode):
+    streams: list[Stream]
+
+
+class OutputNode(Node):
+    streams: list[Stream]
+
+
+class OutputStream(Stream):
+    node: OutputNode | "GlobalNode" | "MergeOutputsNode"
+
+
+class GlobalNode(Node):
+    stream: OutputStream
+
+
+class MergeOutputsNode(Node):
+    streams: list[OutputStream]
