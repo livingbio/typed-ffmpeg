ee30dd865d063a030dfde3dd413ae7a7946ffd7e
lucemia <lucemia@gmail.com>
2024-01-10 11:29:51 +0800

    fix ci

---
+++ src/ffmpeg/base.py
@@ -0,0 +1,18 @@
+from .nodes.input_node import InputNode
+from .streams.av import AVStream
+
+
+def input(filename: str, **kwargs: str | int | None | float) -> AVStream:
+    """Input file URL (ffmpeg ``-i`` option)
+    Any supplied kwargs are passed to ffmpeg verbatim (e.g. ``t=20``,
+    ``f='mp4'``, ``acodec='pcm'``, etc.).
+    To tell ffmpeg to read from stdin, use ``pipe:`` as the filename.
+    Official documentation: `Main options <https://ffmpeg.org/ffmpeg.html#Main-options>`__
+    """
+    kwargs["filename"] = filename
+    fmt = kwargs.pop("f", None)
+    if fmt:
+        if "format" in kwargs:
+            raise ValueError("Can't specify both `format` and `f` kwargs")
+        kwargs["format"] = fmt
+    return InputNode(name=input.__name__, kwargs=kwargs).stream()
