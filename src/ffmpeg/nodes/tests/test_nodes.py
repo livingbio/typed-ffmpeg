1f8e5699e85781519cfa647a78a3501961aa5652
lucemia <lucemia@gmail.com>
2024-01-11 00:20:29 +0800

    merge nodes

--- src/ffmpeg/nodes/tests/test_nodes.py
+++ src/ffmpeg/nodes/tests/test_nodes.py
@@ -1,11 +1,18 @@
 from syrupy.assertion import SnapshotAssertion
 from syrupy.extensions.json import JSONSnapshotExtension
 
-from ..nodes import InputNode
+from ..nodes import InputNode, OutputNode
 
 
 def test_input_node(snapshot: SnapshotAssertion) -> None:
     assert (
         snapshot(extension_class=JSONSnapshotExtension)
         == InputNode(filename="test.mp4", kwargs={"f": "mp4"}).get_args()
+    )
+
+
+def test_output_node(snapshot: SnapshotAssertion) -> None:
+    assert (
+        snapshot(extension_class=JSONSnapshotExtension)
+        == OutputNode(filename="test.mp4", kwargs={"bufsize": "64k"}, inputs=[]).get_args()
     )
