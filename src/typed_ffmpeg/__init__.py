57ad5c8743b66baf4b15b66580530c20171c1d8c
lucemia <lucemia@gmail.com>
2024-01-15 16:50:07 +0800

    fix Error's path

--- src/typed_ffmpeg/__init__.py
+++ src/typed_ffmpeg/__init__.py
@@ -1,6 +1,7 @@
 from . import filters, nodes, streams
 from .base import input, merge_outputs, output
+from .exeptions import Error
 from .probe import probe
 
-__all__ = ["input", "output", "merge_outputs", "probe", "filters"]
+__all__ = ["input", "output", "merge_outputs", "probe", "filters", "Error"]
 __all__ += streams.__all__ + nodes.__all__
