17f9f6ef5209d77e202e7bcf77bae04012321d85
lucemia <lucemia@gmail.com>
2024-01-15 21:57:44 +0800

    adjust nodes interface

---
+++ src/typed_ffmpeg/nodes/__init__.py
@@ -0,0 +1,11 @@
+from .nodes import FilterableStream, FilterNode, GlobalNode, InputNode, MergeOutputsNode, OutputNode, OutputStream
+
+__all__ = [
+    "FilterableStream",
+    "FilterNode",
+    "GlobalNode",
+    "InputNode",
+    "OutputNode",
+    "OutputStream",
+    "MergeOutputsNode",
+]
