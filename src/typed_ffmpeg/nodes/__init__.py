e4322639973706e4290cdf2f1aae969fcfd72335
lucemia <lucemia@gmail.com>
2024-01-15 23:55:44 +0800

    fix import path

--- src/typed_ffmpeg/nodes/__init__.py
+++ src/typed_ffmpeg/nodes/__init__.py
@@ -1,6 +1,9 @@
+from .base import Node, Stream
 from .nodes import FilterableStream, FilterNode, GlobalNode, InputNode, MergeOutputsNode, OutputNode, OutputStream
 
 __all__ = [
+    "Node",
+    "Stream",
     "FilterableStream",
     "FilterNode",
     "GlobalNode",
