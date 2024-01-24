600980cafcfce450255d13f53eced4cc01d8139f
lucemia <lucemia@gmail.com>
2024-01-25 00:18:29 +0800

    fix

--- src/ffmpeg/base.py
+++ src/ffmpeg/base.py
@@ -8,14 +8,12 @@ def input(filename: str, **kwargs: Any) -> AVStream:
     """
     Input file URL (ffmpeg ``-i`` option)
 
-    Parameters:
-    -----------
-    :param filename: Input file URL
-    :param kwargs: ffmpeg options
+    Args:
+        filename (str): Input file URL
+        **kwargs: ffmpeg options
 
     Returns:
-    --------
-    :return: Input stream
+        AVStream: Input stream
     """
     fmt = kwargs.pop("f", None)
     if fmt:
@@ -29,20 +27,27 @@ def output(*streams: FilterableStream, filename: str, **kwargs: Any) -> OutputSt
     """
     Output the streams to a file URL
 
-    Parameters:
-    -----------
-    :param *streams: the streams to output
-    :param str filename: the filename to output to
-    :param Any kwargs: the arguments for the output
+    Args:
+        *streams (FilterableStream): the streams to output
+        filename (str): the filename to output to
+        **kwargs: the arguments for the output
 
     Returns:
-    ---------
-    :returns: the output stream
+        OutputStream: the output stream
     """
     return OutputNode(filename=filename, inputs=streams, kwargs=tuple(kwargs.items())).stream()
 
 
 def merge_outputs(*streams: OutputStream) -> OutputStream:
+    """
+    Merge multiple output streams into one.
+
+    Args:
+        *streams (OutputStream): The output streams to merge.
+
+    Returns:
+        OutputStream: The merged output stream.
+    """
     return MergeOutputsNode(inputs=streams).stream()
 
 
@@ -50,15 +55,13 @@ def filter(*streams: FilterableStream, name: str, **kwargs: Any) -> AVStream:
     """
     Apply a custom filter which has only one output to this stream
 
-    Parameters:
-    -----------
-    :param FilterableStream *streams: the streams to apply the filter to
-    :param str name: the name of the filter
-    :param Any kwargs: the arguments for the filter
+    Args:
+        *streams (FilterableStream): the streams to apply the filter to
+        name (str): the name of the filter
+        **kwargs: the arguments for the filter
 
     Returns:
-    ---------
-    :returns: the output stream
+        AVStream: the output stream
     """
     return FilterNode(name=name, inputs=streams, kwargs=tuple(kwargs.items())).stream(0)
 
@@ -67,15 +70,12 @@ def filter_multi_output(*streams: FilterableStream, name: str, **kwargs: Any) ->
     """
     Apply a custom filter which has multiple outputs to this stream
 
-    Parameters:
-    -----------
-    :param FilterableStream *streams: the streams to apply the filter to
-    :param str name: the name of the filter
-    :param Any kwargs: the arguments for the filter
+    Args:
+        *streams (FilterableStream): the streams to apply the filter to
+        name (str): the name of the filter
+        **kwargs: the arguments for the filter
 
     Returns:
-    ---------
-    :returns: the FilterNode
+        FilterNode: the FilterNode
     """
-
     return FilterNode(name=name, kwargs=tuple(kwargs.items()), inputs=streams)
