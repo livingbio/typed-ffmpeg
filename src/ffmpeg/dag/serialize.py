c399b3359a9c42dc98cbe88ccd6c534e38ffa3ff
David Chen <lucemia@gmail.com>
2024-02-02 11:46:09 +0800

    Fix 47 serialize (#239)
    * move is_dag to dag folder
    
    * move serializer
    
    * fix #47 serialize
    
    * remove not used method
    
    * fix snapshot

---
+++ src/ffmpeg/dag/serialize.py
@@ -0,0 +1,87 @@
+from __future__ import absolute_import, annotations
+
+import datetime
+import json  # noqa
+from dataclasses import fields, is_dataclass
+from enum import Enum
+from typing import Any, ClassVar, Dict, Protocol, TypeVar
+
+
+class IsDataclass(Protocol):
+    # as already noted in comments, checking for this attribute is currently
+    # the most reliable way to ascertain that something is a dataclass
+    __dataclass_fields__: ClassVar[Dict[str, Any]]
+
+
+T = TypeVar("T", bound=IsDataclass)
+
+
+def load_class(path: str) -> Any:
+    module_path, class_name = path.rsplit(".", 1)
+    module = __import__(module_path, fromlist=[class_name])
+    return getattr(module, class_name)
+
+
+class Encoder(json.JSONEncoder):
+    # Extend JSON encoder to support more type, especial for Schematic Model
+    # NOTE: it won't validate schematic model during encode / decode
+
+    def default(self, obj: Any) -> Any:
+        if isinstance(obj, Enum):
+            return {
+                "__class__": f"{obj.__class__.__module__}.{obj.__class__.__name__}",
+                "value": obj.value,
+            }
+        elif isinstance(obj, datetime.datetime):
+            return obj.strftime("%Y-%m-%d %H:%M:%S.%f%z")
+        elif is_dataclass(obj):
+            output = {}
+            for field in fields(obj):
+                v = getattr(obj, field.name)
+                output[field.name] = self.default(v)
+
+            return {
+                "__class__": f"{obj.__class__.__module__}.{obj.__class__.__name__}",
+                **output,
+            }
+        return obj
+
+
+class Decoder(json.JSONDecoder):
+    # Extend JSON decoder to support more type
+
+    def __init__(self, *args: Any, **kwargs: Any):
+        json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
+
+    def frozen(self, v: Any) -> Any:
+        if isinstance(v, list):
+            return tuple(self.frozen(i) for i in v)
+
+        if isinstance(v, dict):
+            return tuple((key, self.frozen(value)) for key, value in v.items())
+
+        return v
+
+    def object_hook(self, obj: Any) -> Any:  # pylint: disable=method-hidden
+        if isinstance(obj, dict):
+            if obj.get("__class__"):
+                cls = load_class(obj.pop("__class__"))
+
+                if is_dataclass(cls):
+                    # NOTE: in our application, the dataclass is always frozen
+                    return cls(**{k: self.frozen(v) for k, v in obj.items()})
+
+                return cls(**{k: v for k, v in obj.items()})
+
+        return obj
+
+
+def loads(cls: type[T], raw: str) -> T:
+    data = json.loads(raw, cls=Decoder)
+    assert isinstance(data, cls)
+    return data
+
+
+# Serialization
+def dumps(instance: IsDataclass) -> str:
+    return json.dumps(instance, cls=Encoder)
---
+++ src/ffmpeg/dag/tests/test_serialize.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass
+
+from syrupy.assertion import SnapshotAssertion
+
+from ...base import input
+from ...filters import concat
+from ..serialize import dumps, loads
+
+
+# Define your dataclasses
+@dataclass
+class Address:
+    street: str
+    city: str
+
+
+@dataclass
+class Person:
+    name: str
+    age: int
+    address: Address
+
+
+def test_load_and_dump(snapshot: SnapshotAssertion) -> None:
+    # Example usage
+    person = Person("John Doe", 30, Address("123 Main St", "Anytown"))
+    serialized = dumps(person)
+    assert snapshot(name="serialized") == serialized
+
+    deserialized = loads(Person, serialized)
+
+    assert isinstance(deserialized, Person)
+    assert snapshot(name="deserialized") == deserialized
+    assert person == deserialized
+
+
+def test_load_and_dump_on_complex_filter(snapshot: SnapshotAssertion) -> None:
+    in_file = input("input.mp4")
+    overlay_file = input("overlay.png")
+    stream = (
+        concat(
+            in_file.trim(start_frame=10, end_frame=20),
+            in_file.trim(start_frame=30, end_frame=40),
+        )
+        .video(0)
+        .overlay(overlay_file.hflip())
+        .drawbox(x="50", y="50", width="120", height="120", color="red", thickness="5")
+        .output(filename="out.mp4")
+    )
+    serialized = dumps(stream)
+    assert snapshot(name="serialized") == serialized
+
+    deserialized = loads(type(stream), serialized)
+
+    assert isinstance(deserialized, type(stream))
+    assert stream == deserialized
