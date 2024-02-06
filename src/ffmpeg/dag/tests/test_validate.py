ebbb400a68c075239083323b6f5538850e41bdbf
lucemia <lucemia@gmail.com>
2024-02-06 23:48:35 +0800

    fix ci

--- src/ffmpeg/dag/tests/test_validate.py
+++ src/ffmpeg/dag/tests/test_validate.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 import pytest
 from syrupy.assertion import SnapshotAssertion
 
@@ -5,12 +7,12 @@ from ...base import input
 from ...filters import concat
 from ..context import DAGContext
 from ..nodes import OutputStream
-from ..validate import validate
+from ..validate import _validate_not_utilize_split, _validate_reuse_stream
 
 
-def do_validate(output: OutputStream) -> None:
+def do_validate(output: OutputStream, func: Callable[[DAGContext], DAGContext]) -> DAGContext:
     context = DAGContext.build(output.node)
-    validate(context)
+    return func(context)
 
 
 def test_validate_reuse_stream(snapshot: SnapshotAssertion) -> None:
@@ -18,7 +20,7 @@ def test_validate_reuse_stream(snapshot: SnapshotAssertion) -> None:
 
     with pytest.raises(AssertionError) as e:
         rev = input1.reverse()
-        do_validate(concat(rev.trim(), rev.trim()).video(0).output(filename="tmp.mp4"))
+        do_validate(concat(rev.trim(), rev.trim()).video(0).output(filename="tmp.mp4"), _validate_reuse_stream)
 
     assert snapshot == e
 
@@ -27,7 +29,7 @@ def test_validate_not_utilize_split(snapshot: SnapshotAssertion) -> None:
     input1 = input("input1.mp4")
 
     with pytest.raises(AssertionError) as e:
-        do_validate(input1.split(outputs=2).video(0).output(filename="tmp.mp4"))
+        do_validate(input1.split(outputs=2).video(0).output(filename="tmp.mp4"), _validate_not_utilize_split)
 
     assert snapshot == e
 
@@ -36,7 +38,7 @@ def test_reduntant_split_outputs_1(snapshot: SnapshotAssertion) -> None:
     input1 = input("input1.mp4")
 
     with pytest.raises(AssertionError) as e:
-        do_validate(input1.split(outputs=1).video(0).output(filename="tmp.mp4"))
+        do_validate(input1.split(outputs=1).video(0).output(filename="tmp.mp4"), _validate_not_utilize_split)
 
     assert snapshot == e
 
@@ -51,6 +53,6 @@ def test_reduntant_split_duplicate(snapshot: SnapshotAssertion) -> None:
     s01 = s0.split(outputs=2).video(1)
 
     with pytest.raises(AssertionError) as e:
-        do_validate(concat(s00, s01, s1, n=3).video(0).output(filename="tmp.mp4"))
+        do_validate(concat(s00, s01, s1, n=3).video(0).output(filename="tmp.mp4"), _validate_not_utilize_split)
 
     assert snapshot == e
