34aee2f85b4fb2e15cbf69849a6f4d985fee478d
lucemia <lucemia@gmail.com>
2024-02-09 02:27:25 +0800

    add remove split function

--- src/ffmpeg/dag/tests/test_validate.py
+++ src/ffmpeg/dag/tests/test_validate.py
@@ -9,43 +9,42 @@ from syrupy.extensions.json import JSONSnapshotExtension
 from ...base import input
 from ...filters import concat
 from ..context import DAGContext
-from ..schema import Node
-from ..validate import _validate_reused_stream
-
-# def test_validate_not_utilize_split(snapshot: SnapshotAssertion) -> None:
-#     input1 = input("input1.mp4")
-
-#     context = DAGContext.build(input1.split(outputs=2).video(0).output(filename="tmp.mp4"))
-#     with pytest.raises(AssertionError) as e:
-#         _validate_not_utilize_split(context)
-
-#     assert snapshot == e
+from ..schema import Node, Stream
+from ..validate import _validate_reused_stream, remove_split
 
 
-# def test_reduntant_split_outputs_1(snapshot: SnapshotAssertion) -> None:
-#     input1 = input("input1.mp4")
+def not_utilize_split() -> Any:
+    input1 = input("input1.mp4")
 
-#     context = DAGContext.build(input1.split(outputs=1).video(0).output(filename="tmp.mp4"))
-#     with pytest.raises(AssertionError) as e:
-#         _validate_not_utilize_split(context)
-
-#     assert snapshot == e
+    return pytest.param(input1.split(outputs=2).video(0).output(filename="tmp.mp4"), id="not-utilize-split")
 
 
-# def test_reduntant_split_duplicate(snapshot: SnapshotAssertion) -> None:
-#     input1 = input("input1.mp4")
-#     s = input1.split(outputs=2)
-#     s0 = s.video(0)
-#     s1 = s.video(1)
+def redundant_split_outputs_1() -> Any:
+    input1 = input("input1.mp4")
+    graph = input1.split(outputs=1).video(0).output(filename="tmp.mp4")
+    return pytest.param(graph, id="reduntant-split-outputs-1")
 
-#     s00 = s0.split(outputs=2).video(0)
-#     s01 = s0.split(outputs=2).video(1)
 
-#     context = DAGContext.build(concat(s00, s01, s1, n=3).video(0).output(filename="tmp.mp4"))
-#     with pytest.raises(AssertionError) as e:
-#         _validate_not_utilize_split(context)
+def reduntant_split_duplicate() -> Any:
+    input1 = input("input1.mp4")
+    s = input1.split(outputs=2)
+    s0 = s.video(0)
+    s1 = s.video(1)
 
-#     assert snapshot == e
+    s00 = s0.split(outputs=2).video(0)
+    s01 = s0.split(outputs=2).video(1)
+
+    graph = concat(s00, s01, s1, n=3).video(0).output(filename="tmp.mp4")
+    return pytest.param(graph, id="reduntant-split-duplicate")
+
+
+@pytest.mark.parametrize("graph", [reduntant_split_duplicate(), redundant_split_outputs_1(), not_utilize_split()])
+def test_rebuild_graph(graph: Stream, drawer: Callable[[str, Node], Path]) -> None:
+    drawer("before", graph.node)
+    rebuilded = remove_split(graph)
+    drawer("after", rebuilded[0].node)
+
+
 class Validator(Protocol):
     def __call__(self, context: DAGContext = ..., auto_fix: bool = False) -> DAGContext:
         ...
@@ -72,19 +71,19 @@ def reuse_multi_stream() -> Any:
     input1 = input("input1.mp4")
     rev = input1.reverse()
 
-    graph = concat(concat(rev, rev).video(0), concat(rev, rev, rev, n=3).video(0))
+    graph = concat(concat(rev, rev).video(0), concat(rev, rev, rev, n=3).video(0)).video(0)
     return pytest.param(graph, _validate_reused_stream, id="reuse-multi-stream")
 
 
 @pytest.mark.parametrize("graph, validator", [reuse_stream_same_node(), reuse_stream_two_node(), reuse_multi_stream()])
 def test_validate(
-    graph: Node,
+    graph: Stream,
     validator: Validator,
     snapshot: SnapshotAssertion,
     drawer: Callable[[str, Node], Path],
 ) -> None:
 
-    drawer("before", graph)
+    drawer("before", graph.node)
     assert snapshot(name="before", extension_class=JSONSnapshotExtension) == asdict(graph)
 
     context = DAGContext.build(graph)
--- src/ffmpeg/dag/validate.py
+++ src/ffmpeg/dag/validate.py
@@ -10,6 +10,50 @@ from .context import DAGContext
 from .nodes import FilterNode
 
 
+def remove_split(current_stream: Stream, mapping: dict[Stream, Stream] = None) -> tuple[Stream, dict[Stream, Stream]]:
+    """
+    Rebuild the graph with the given mapping.
+
+    Args:
+        node: The node to rebuild the graph from.
+        mapping: The mapping to rebuild the graph with.
+
+    Returns:
+        A tuple of the new node and the new mapping.
+    """
+
+    # remove all split nodes
+    # add split nodes to the graph
+    if mapping is None:
+        mapping = {}
+
+    if current_stream in mapping:
+        return mapping[current_stream], mapping
+
+    if not current_stream.node.inputs:
+        mapping[current_stream] = current_stream
+        return current_stream, mapping
+
+    if isinstance(current_stream.node, FilterNode):
+        # if the current node is a split node, we need to remove it
+        if current_stream.node.name in ("split", "asplit"):
+            new_stream, _mapping = remove_split(current_stream=current_stream.node.inputs[0], mapping=mapping)
+            mapping[current_stream] = mapping[current_stream.node.inputs[0]]
+            return mapping[current_stream.node.inputs[0]], mapping
+
+    inputs = []
+    for input_stream in sorted(current_stream.node.inputs, key=lambda stream: -len(stream.node.upstream_nodes)):
+        new_stream, _mapping = remove_split(current_stream=input_stream, mapping=mapping)
+        inputs.append(new_stream)
+        mapping |= _mapping
+
+    new_node = replace(current_stream.node, inputs=tuple(inputs))
+    new_stream = replace(current_stream, node=new_node)
+
+    mapping[current_stream] = new_stream
+    return new_stream, mapping
+
+
 def _validate_reused_stream(context: DAGContext, auto_fix: bool = False) -> DAGContext:
     """
     Validate that no stream is reused by multiple nodes.
@@ -57,48 +101,62 @@ def _validate_reused_stream(context: DAGContext, auto_fix: bool = False) -> DAGC
             new_node = replace(node, inputs=tuple(inputs))
             current_node = current_node.replace(node, new_node)
 
+    # NOTE: do we need to fix the reused streams in the new context?
     return DAGContext.build(current_node)
 
 
-def _validate_not_utilize_split(context: DAGContext) -> DAGContext:
-    """
-    Validate that split nodes are utilized.
+# def _validate_not_utilize_split(context: DAGContext, auto_fix: bool = False) -> DAGContext:
+#     """
+#     Validate that split nodes are utilized.
 
-    Args:
-        context: The DAG context to validate.
+#     Args:
+#         context: The DAG context to validate.
 
-    Returns:
-        The validated DAG context.
-    """
+#     Returns:
+#         The validated DAG context.
+#     """
 
-    all_split_node = [
-        node for node in context.all_nodes if isinstance(node, FilterNode) and node.name in ("split", "asplit")
-    ]
+#     all_split_node = [
+#         node for node in context.all_nodes if isinstance(node, FilterNode) and node.name in ("split", "asplit")
+#     ]
 
-    not_utilize_splits = [
-        node
-        for node in all_split_node
-        if len(context.get_outgoing_streams(node)) < int(dict(node.kwargs).get("outputs", 2))
-    ]
+#     not_utilize_splits = [
+#         node
+#         for node in all_split_node
+#         if len(context.get_outgoing_streams(node)) < int(dict(node.kwargs).get("outputs", 2))
+#     ]
 
-    assert not not_utilize_splits, f"Found not utilized split nodes: {not_utilize_splits}"
+#     if not auto_fix:
+#         assert not not_utilize_splits, f"Found not utilized split nodes: {not_utilize_splits}"
+#         return context
 
-    # if a split node has only one output, it is reduntant
-    reduntant_splits = {node for node in all_split_node if int(dict(node.kwargs).get("outputs", 2) == 1)}
+#     for node in sorted(not_utilize_splits, key=lambda node: -len(node.upstream_nodes)):
+#         # NOTE: auto fix the not utilized split nodes
+#         count = len(context.get_outgoing_streams(node))
 
-    # if a split node's parent is also a split node, it is reduntant
-    reduntant_splits |= {
-        node
-        for node in all_split_node
-        if isinstance(node.inputs[0].node, FilterNode) and node.inputs[0].node.name in ("split", "asplit")
-    }
+#         if isinstance(node.inputs[0], VideoStream):
+#             new_split_node = node.inputs[0].split(outputs=count)
+#         elif isinstance(node.inputs[0], AudioStream):
+#             new_split_node = node.inputs[0].asplit(outputs=count)
+#         else:
+#             raise ValueError(f"Invalid split node: {node}")
 
-    # NOTE:
-    # if not all split outstream used, it is reduntant but is valid.
+#     # # if a split node has only one output, it is reduntant
+#     # reduntant_splits = {node for node in all_split_node if int(dict(node.kwargs).get("outputs", 2) == 1)}
 
-    assert not reduntant_splits, f"Found reduntant split nodes: {reduntant_splits}"
+#     # # if a split node's parent is also a split node, it is reduntant
+#     # reduntant_splits |= {
+#     #     node
+#     #     for node in all_split_node
+#     #     if isinstance(node.inputs[0].node, FilterNode) and node.inputs[0].node.name in ("split", "asplit")
+#     # }
 
-    return context
+#     # # NOTE:
+#     # # if not all split outstream used, it is reduntant but is valid.
+
+#     # assert not reduntant_splits, f"Found reduntant split nodes: {reduntant_splits}"
+
+#     # return context
 
 
 def validate(context: DAGContext) -> DAGContext:
