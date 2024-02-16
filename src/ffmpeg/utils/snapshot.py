82ea0ce6e79789a970664b6f3925c5d16b78822a
lucemia <lucemia@gmail.com>
2024-02-17 04:49:06 +0800

    disable imagehash

--- src/ffmpeg/utils/snapshot.py
+++ src/ffmpeg/utils/snapshot.py
@@ -1,8 +1,5 @@
-from io import BytesIO
 from typing import Optional
 
-import imagehash
-from PIL import Image
 from syrupy.extensions.image import PNGImageSnapshotExtension
 from syrupy.types import PropertyFilter, PropertyMatcher, SerializableData, SerializedData
 
@@ -24,21 +21,21 @@ class DAGSnapshotExtenstion(PNGImageSnapshotExtension):
         with open(graph_path, "rb") as ifile:
             return super().serialize(ifile.read())
 
-    def matches(
-        self,
-        *,
-        serialized_data: "SerializableData",
-        snapshot_data: "SerializableData",
-    ) -> bool:
-        """
-        Compares serialized data and snapshot data and returns
-        whether they match.
-        """
-        snapshot_image = Image.open(BytesIO(snapshot_data))
-        serialized_image = Image.open(BytesIO(serialized_data))
+    # def matches(
+    #     self,
+    #     *,
+    #     serialized_data: "SerializableData",
+    #     snapshot_data: "SerializableData",
+    # ) -> bool:
+    #     """
+    #     Compares serialized data and snapshot data and returns
+    #     whether they match.
+    #     """
+    #     snapshot_image = Image.open(BytesIO(snapshot_data))
+    #     serialized_image = Image.open(BytesIO(serialized_data))
 
-        snapshot_hash = imagehash.average_hash(snapshot_image)
-        serialized_hash = imagehash.average_hash(serialized_image)
+    #     snapshot_hash = imagehash.average_hash(snapshot_image)
+    #     serialized_hash = imagehash.average_hash(serialized_image)
 
-        print(f"snapshot_hash: {snapshot_hash} serialized_hash: {serialized_hash}")
-        return snapshot_hash == serialized_hash
+    #     print(f"snapshot_hash: {snapshot_hash} serialized_hash: {serialized_hash}")
+    #     return snapshot_hash == serialized_hash
