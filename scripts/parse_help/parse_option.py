00bd000bd836347b4c5779f9998aaafc9d4a6e81
lucemia <lucemia@gmail.com>
2024-02-22 17:11:56 +0800

    update

--- scripts/parse_help/parse_option.py
+++ scripts/parse_help/parse_option.py
@@ -1,6 +1,13 @@
 import subprocess
+from typing import Any
 
 
 def help_full_text() -> str:
     result = subprocess.run(["ffmpeg", "-h", "full", "-hide_banner"], stdout=subprocess.PIPE, text=True)
     return result.stdout
+
+
+def process() -> Any:
+    text = help_full_text()
+
+    return [k.strip() for k in text.split("\n\n")]
--- scripts/parse_help/tests/test_parse_option.py
+++ scripts/parse_help/tests/test_parse_option.py
@@ -1,11 +1,20 @@
 from syrupy.assertion import SnapshotAssertion
 from syrupy.extensions.json import JSONSnapshotExtension
+from syrupy.extensions.single_file import SingleFileSnapshotExtension
 
 from ..parse_filter import parse_section_tree
-from ..parse_option import help_full_text
+from ..parse_option import help_full_text, process
 
 
 def test_help_full_text(snapshot: SnapshotAssertion) -> None:
+    assert snapshot(name="help-full-text", extension_class=SingleFileSnapshotExtension) == help_full_text().encode(
+        "utf8"
+    )
+
     assert snapshot(name="help-full-text", extension_class=JSONSnapshotExtension) == parse_section_tree(
         help_full_text()
     )
+
+
+def test_process(snapshot: SnapshotAssertion) -> None:
+    assert snapshot(name="process", extension_class=JSONSnapshotExtension) == process()
