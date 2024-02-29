da788555665be67010d9593d94916b3a14960ce5
lucemia <lucemia@gmail.com>
2024-02-29 22:30:28 +0800

    udpate test case

--- src/ffmpeg/utils/lazy_eval/tests/test_lazy_eval.py
+++ src/ffmpeg/utils/lazy_eval/tests/test_lazy_eval.py
@@ -1,27 +1,56 @@
 from __future__ import annotations
 
 from json import loads
-from typing import Generator
 
 import pytest
 from syrupy.assertion import SnapshotAssertion
 from syrupy.extensions.json import JSONSnapshotExtension
 
 from ....dag.serialize import dumps
-from ..schema import PlaceHolder
+from ..schema import LazyOperator, PlaceHolder
+
+VarX = PlaceHolder(key="X")
+
+VarY = PlaceHolder(key="Y")
 
 
-@pytest.fixture
-def VarX() -> Generator[PlaceHolder, None, None]:
-    yield PlaceHolder(key="X")
+@pytest.mark.parametrize(
+    "oper",
+    [
+        VarX + 1,
+        1 + VarX,
+        VarX + VarY,
+        VarX - 1,
+        1 - VarX,
+        VarX - VarY,
+        VarX * 1,
+        1 * VarX,
+        VarX * VarY,
+        VarX / 1,
+        1 / VarX,
+        VarX / VarY,
+        VarX**1,
+        1**VarX,
+        VarX**VarY,
+        -VarX,
+        +VarX,
+        abs(VarX),
+        VarX % 1,
+        1 % VarX,
+        VarX % VarY,
+        VarX // 1,
+        1 // VarX,
+        VarX // VarY,
+    ],
+)
+def test_operator(snapshot: SnapshotAssertion, oper: LazyOperator) -> None:
+    assert snapshot(extension_class=JSONSnapshotExtension) == loads(dumps(oper))
+    assert snapshot(name="repr") == str(oper)
+
+    assert snapshot == oper.eval(**{VarX.key: 3, VarY.key: 5})
 
 
-@pytest.fixture
-def VarY() -> Generator[PlaceHolder, None, None]:
-    yield PlaceHolder(key="Y")
-
-
-def test_lazyoperator_number(snapshot: SnapshotAssertion, VarX: PlaceHolder, VarY: PlaceHolder) -> None:
+def test_lazyoperator_number(snapshot: SnapshotAssertion) -> None:
     Op = ((1 + VarX + VarY - 5) * 3) / 4**2 * (-VarX) + (+VarY) + (abs(VarX)) + (VarX % VarY) + (VarX // VarY)
 
     assert snapshot(name="repr") == str(Op)
@@ -34,5 +63,6 @@ def test_lazyoperator_number(snapshot: SnapshotAssertion, VarX: PlaceHolder, Var
     with pytest.raises(KeyError):
         Op.eval(**{VarX.key: 30})
 
+    assert Op.ready() is False
     assert snapshot == Op.eval(**{VarX.key: 30, VarY.key: 45})
     assert snapshot == Op
