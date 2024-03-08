3bd47a90bdb421b435b572a9422d5e98d0d1cd9f
lucemia <lucemia@gmail.com>
2024-03-08 07:48:08 +0000

    fix

--- src/ffmpeg/scripts/parse_c/tests/test_pre_compile.py
+++ src/ffmpeg/scripts/parse_c/tests/test_pre_compile.py
@@ -1,9 +1,5 @@
-from syrupy.assertion import SnapshotAssertion
-from syrupy.extensions.single_file import SingleFileSnapshotExtension
-
 from ..pre_compile import precompile
 
 
 def test_pre_compile_file() -> None:
     precompile()
-    
