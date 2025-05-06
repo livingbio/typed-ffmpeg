06be7283797b6551f006be640442b703801d5c0b
lucemia <lucemia@gmail.com>
2025-05-06 07:42:13 +0000

    chore: configure eslint to work with prettier

--- ffmpeg-flow-editor/eslint.config.js
+++ ffmpeg-flow-editor/eslint.config.js
@@ -3,11 +3,12 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, prettier],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
