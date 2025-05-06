5aa738bafe06036b1f537ed47585a7787e8ea3cb
lucemia <lucemia@gmail.com>
2025-05-06 08:26:12 +0000

    ref #512 add DAG ts

---
+++ ffmpeg-flow-editor/src/types/dag.ts
@@ -0,0 +1,118 @@
+import { FFmpegFilter } from './ffmpeg';
+import { Serializable, serializable } from '../utils/serialize';
+
+// Core types
+export enum StreamType {
+  audio = 'audio',
+  video = 'video',
+}
+
+// Base interfaces
+export interface Node {
+  kwargs: Record<string, string | number | boolean>;
+  inputs: Stream[];
+}
+
+export interface Stream {
+  node: Node;
+  index: number | null;
+}
+
+// Node types
+@serializable
+export class FilterNode extends Serializable implements Node {
+  constructor(
+    public name: string,
+    public inputs: FilterableStream[],
+    public input_typings: StreamType[],
+    public output_typings: StreamType[],
+    public filter: FFmpegFilter,
+    public kwargs: Record<string, string | number | boolean> = {}
+  ) {
+    super();
+  }
+}
+
+@serializable
+export class InputNode extends Serializable implements Node {
+  constructor(
+    public filename: string,
+    public inputs: [] = [],
+    public kwargs: Record<string, string | number | boolean> = {}
+  ) {
+    super();
+  }
+}
+
+@serializable
+export class OutputNode extends Serializable implements Node {
+  constructor(
+    public filename: string,
+    public inputs: FilterableStream[],
+    public kwargs: Record<string, string | number | boolean> = {}
+  ) {
+    super();
+  }
+}
+
+@serializable
+export class GlobalNode extends Serializable implements Node {
+  constructor(
+    public inputs: OutputStream[],
+    public kwargs: Record<string, string | number | boolean> = {}
+  ) {
+    super();
+  }
+}
+
+// Stream types
+@serializable
+export class FilterableStream extends Serializable implements Stream {
+  constructor(
+    public node: FilterNode | InputNode,
+    public index: number | null = null
+  ) {
+    super();
+  }
+}
+
+@serializable
+export class VideoStream extends FilterableStream {
+  constructor(node: FilterNode | InputNode, index: number | null = null) {
+    super(node, index);
+  }
+}
+
+@serializable
+export class AudioStream extends FilterableStream {
+  constructor(node: FilterNode | InputNode, index: number | null = null) {
+    super(node, index);
+  }
+}
+
+@serializable
+export class AVStream extends FilterableStream {
+  constructor(node: FilterNode | InputNode, index: number | null = null) {
+    super(node, index);
+  }
+}
+
+@serializable
+export class OutputStream extends Serializable implements Stream {
+  constructor(
+    public node: OutputNode,
+    public index: number | null = null
+  ) {
+    super();
+  }
+}
+
+@serializable
+export class GlobalStream extends Serializable implements Stream {
+  constructor(
+    public node: GlobalNode,
+    public index: number | null = null
+  ) {
+    super();
+  }
+}
