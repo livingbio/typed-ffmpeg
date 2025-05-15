1116d7d804d7cf58c3142dd27b3c46d7c7caa645
lucemia <lucemia@gmail.com>
2025-05-15 09:03:01 +0000

    update docstrings

--- src/ffmpeg/streams/av.py
+++ src/ffmpeg/streams/av.py
@@ -12,14 +12,44 @@ class AVStream(AudioStream, VideoStream):
 
     @property
     def video(self) -> VideoStream:
+        """
+        Get the video stream from the input node.
+
+        Returns:
+            VideoStream: The video stream from the input node.
+        """
         return VideoStream(node=self.node, index=self.index)
 
     @property
     def audio(self) -> AudioStream:
+        """
+        Get the audio stream from the input node.
+
+        Returns:
+            AudioStream: The audio stream from the input node.
+        """
         return AudioStream(node=self.node, index=self.index)
 
     def video_stream(self, index: int) -> VideoStream:
+        """
+        Get the video stream from the input node. with a specific index.
+
+        Args:
+            index: The index of the video stream.
+
+        Returns:
+            VideoStream: The video stream from the input node.
+        """
         return VideoStream(node=self.node, index=index)
 
     def audio_stream(self, index: int) -> AudioStream:
+        """
+        Get the audio stream from the input node. with a specific index.
+
+        Args:
+            index: The index of the audio stream.
+
+        Returns:
+            AudioStream: The audio stream from the input node.
+        """
         return AudioStream(node=self.node, index=index)
