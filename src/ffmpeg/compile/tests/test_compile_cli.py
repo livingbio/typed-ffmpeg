40a3285f2b6ae47ac73d315ab7c7994e432468ce
David Chen <lucemia@gmail.com>
2025-05-16 12:14:04 +0800

    fix #93 ffmpeg cli parser (#566)
    * fix
    
    * fix
    
    * fix
    
    * better compare
    
    * improve docs
    
    * add parse_global
    
    * pass all test case

--- src/ffmpeg/compile/tests/test_compile_cli.py
+++ src/ffmpeg/compile/tests/test_compile_cli.py
@@ -3,7 +3,7 @@ from syrupy.assertion import SnapshotAssertion
 from syrupy.extensions.json import JSONSnapshotExtension
 
 from ...dag.schema import Stream
-from ..compile_cli import compile_as_list
+from ..compile_cli import compile, compile_as_list, parse
 from .cases import shared_cases
 
 
@@ -12,3 +12,14 @@ def test_compile_cli(snapshot: SnapshotAssertion, graph: Stream) -> None:
     assert snapshot(
         name="compile-cli", extension_class=JSONSnapshotExtension
     ) == compile_as_list(graph)
+
+
+@pytest.mark.parametrize("graph", shared_cases)
+def test_parse_compile(snapshot: SnapshotAssertion, graph: Stream) -> None:
+    compiled = compile(graph)
+    parsed = parse(compiled)
+    compiled_again = compile(parsed)
+    assert snapshot(name="parse-compile", extension_class=JSONSnapshotExtension) == (
+        compiled,
+        compiled_again,
+    )
