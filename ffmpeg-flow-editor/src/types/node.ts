079d07fd1f68faef85c608f808e75be89fdcd53b
lucemia <lucemia@gmail.com>
2025-05-20 01:26:16 +0000

    defined NodeType

--- ffmpeg-flow-editor/src/types/node.ts
+++ ffmpeg-flow-editor/src/types/node.ts
@@ -1,14 +1,15 @@
-import { EdgeType } from "./edge";
+import { EdgeType } from './edge';
+
+export type NodeType = 'input' | 'filter' | 'output' | 'global';
 
 export interface NodeData {
-    label: string;
-    nodeType: 'input' | 'filter' | 'output' | 'global';
-    filterName?: string;
-    filename?: string;
-    parameters?: Record<string, string>;
-    handles: {
-        inputs: { id: string; type: EdgeType }[];
-        outputs: { id: string; type: EdgeType }[];
-    };
-  }
-  
+  label: string;
+  nodeType: NodeType;
+  filterName?: string;
+  filename?: string;
+  parameters?: Record<string, string>;
+  handles: {
+    inputs: { id: string; type: EdgeType }[];
+    outputs: { id: string; type: EdgeType }[];
+  };
+}
