5fb50c5e3f534f7f336688a47610f90466198944
lucemia <lucemia@gmail.com>
2025-06-03 09:25:42 +0000

    support ffmpeg.exe

--- src/ffmpeg/compile/tests/test_compile_cli.py
+++ src/ffmpeg/compile/tests/test_compile_cli.py
@@ -23,3 +23,9 @@ def test_parse_compile(snapshot: SnapshotAssertion, graph: Stream) -> None:
         compiled,
         compiled_again,
     )
+
+
+def test_parse_filter_chain(snapshot: SnapshotAssertion) -> None:
+    parsed = parse(
+        """ffmpeg.exe -nostdin -analyzeduration 100M -probesize 100M -i "input_video.mkv" -vf "scale=-1:-1:flags=lanczos,pad=1920:1080:-1:-1,subtitles='subtitles.srt':stream_index=0:force_style='Fontname=Gotham Rounded Medium,Fontsize=17,Alignment=2,PrimaryColour=&Hffffff&,MarginV=25'" -sn -map 0:a:0 -map 0:v:0 -c:a aac -b:a 192k -ac 2 -c:v libx264 -crf 18 -preset slow -tune film -aq-strength 1.8 -mbtree 0 -pix_fmt yuv420p -y -hide_banner -loglevel error -stats -map_metadata -1 -map_chapters -1 "output_video.mp4" """
+    )
