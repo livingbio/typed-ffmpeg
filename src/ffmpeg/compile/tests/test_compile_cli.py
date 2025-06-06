7b3781ff973f2705a572ef8369c7c0a3edac8c22
lucemia <lucemia@gmail.com>
2025-06-06 02:22:37 +0000

    fix #631 parse drawtext with special character

--- src/ffmpeg/compile/tests/test_compile_cli.py
+++ src/ffmpeg/compile/tests/test_compile_cli.py
@@ -48,6 +48,10 @@ def test_parse_compile(snapshot: SnapshotAssertion, graph: Stream) -> None:
             "ffmpeg -y -not-exist-global -nostdin -i input_video.mkv -not-exist-input -i input-2.mp4 -not-exist-output -b:v 1000k -b:a 128k output_video.mp4",
             id="ignore_not_exist_option",
         ),
+        pytest.param(
+            """ffmpeg -i input.mov -filter_complex "[0:v]drawtext=text='=\\\\;\\:':borderw=2:fontcolor=white:fontfile=resources/fonts/arial.ttf:fontsize=30:r=25/1:timecode=00\\:00\\:00\\:00:x=(W-tw)/2:y=text_h[out]" -map [out] output.mp4""",
+            id="escaping",
+        ),
     ],
 )
 def test_parse_ffmpeg_commands(snapshot: SnapshotAssertion, command: str) -> None:
