4379cbc227ed134ecca8e35055aea2a407081fac
David Chen <lucemia@gmail.com>
2025-06-10 20:20:56 +0800

    fix #646 export subtitle stream (#647)

--- src/ffmpeg/streams/__init__.py
+++ src/ffmpeg/streams/__init__.py
@@ -1,5 +1,6 @@
 from .audio import AudioStream
 from .av import AVStream
+from .subtitle import SubtitleStream
 from .video import VideoStream
 
-__all__ = ["AudioStream", "VideoStream", "AVStream"]
+__all__ = ["AudioStream", "VideoStream", "AVStream", "SubtitleStream"]
