a517e7f3194ed855808b0645ffd364a612fa9e46
lucemia <lucemia@gmail.com>
2025-06-24 08:43:44 +0000

    backport new parse-help

---
+++ src/scripts/code_gen/schema.py
@@ -0,0 +1,23 @@
+from typing import Literal
+
+FFMpegOptionType = Literal[
+    "boolean",
+    "duration",
+    "color",
+    "flags",
+    "dictionary",
+    "pix_fmt",
+    "int",
+    "int64",
+    "double",
+    "float",
+    "string",
+    "video_rate",
+    "image_size",
+    "rational",
+    "sample_fmt",
+    "binary",
+    "channel_layout",
+    "pixel_format",
+    "sample_rate",
+]
