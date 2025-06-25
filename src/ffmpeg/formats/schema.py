8431a53b92a827fa4ee33aaa92ce25026589c5ea
lucemia <lucemia@gmail.com>
2025-06-25 12:37:28 +0000

    refactor ffmpeg option

--- src/ffmpeg/formats/schema.py
+++ src/ffmpeg/formats/schema.py
@@ -1,17 +1,11 @@
 from dataclasses import dataclass
 
-from ..common.serialize import Serializable
-from ..utils.frozendict import FrozenDict
+from ..schema import FFMpegOptionGroup
 
 
 @dataclass(frozen=True, kw_only=True)
-class FFMpegFormatOption(Serializable):
-    kwargs: FrozenDict[str, str | int | float | bool] = FrozenDict({})
+class FFMpegMuxerOption(FFMpegOptionGroup): ...
 
 
 @dataclass(frozen=True, kw_only=True)
-class FFMpegMuxerOption(FFMpegFormatOption): ...
-
-
-@dataclass(frozen=True, kw_only=True)
-class FFMpegDemuxerOption(FFMpegFormatOption): ...
+class FFMpegDemuxerOption(FFMpegOptionGroup): ...
