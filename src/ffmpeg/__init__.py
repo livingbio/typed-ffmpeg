c18088cf2a4f4c7bb0dcb919d6f5de63b160fb9c
lucemia <lucemia@gmail.com>
2025-06-25 17:24:41 +0000

    update

--- src/ffmpeg/__init__.py
+++ src/ffmpeg/__init__.py
@@ -22,7 +22,7 @@ Example:
     ```
 """
 
-from . import codecs, compile, dag, filters, formats, sources
+from . import codecs, compile, dag, filters, formats, options, sources
 from .base import afilter, filter_multi_output, input, merge_outputs, output, vfilter
 from .dag import Stream
 from .exceptions import FFMpegExecuteError, FFMpegTypeError, FFMpegValueError
@@ -56,4 +56,5 @@ __all__ = [
     "get_codecs",
     "get_decoders",
     "get_encoders",
+    "options",
 ]
