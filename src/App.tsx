1909a827a709f0185f7fbb911ff425f308a49220
lucemia <lucemia@gmail.com>
2026-03-30 09:08:18 +0800

    Squashed 'packages/playground/' content from commit b279a9ab
    git-subtree-dir: packages/playground
    git-subtree-split: b279a9abb6a44518fb936012834ce72ae690e518

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import FFmpegFlowEditor from './components/FFmpegFlowEditor';
+
+const theme = createTheme();
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <FFmpegFlowEditor />
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/typed-ffmpeg-playground/',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+  },
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
+});
